f4eb9df92b13ee3753a4578944235be6f315a1d3
sokayna <raja.laaguel1999@gmail.com>
2022-03-18 22:14:55 +0100

    Initial commit

---
+++ annotations/TestList.java
@@ -0,0 +1,14 @@
+package ma.education.tp5.annotations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestList {
+		public static void main(String[] args) {
+		List<Integer> list1= new ArrayList<Integer>();
+		list1.add(12);
+		list1.add(23);
+		list1.add(23);
+		list1.add(12);
+	}
+}
